363e3eee02a97390d6b034ef3f43992665ff17fe
Arowolo Adetunlese <adetunlesearowolo@gmail.com>
2026-02-21 17:36:56 +0100

    requirement text

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from filter_service import filter_router
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["*","localhost:5173","http://localhost:5173"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
